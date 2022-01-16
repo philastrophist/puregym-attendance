163bc54771b8a8ca47bc85f79b2f20dbe22cb776
Shaun Read <shaun.c.read@gmail.com>
2022-01-16 14:00:36 +0000

    show

--- record.py
+++ record.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     parser.add_argument('email')
     parser.add_argument('pin')
     parser.add_argument('--gym', default=None)
+    parser.add_argument('--show', action='store_true')
     args = parser.parse_args()
     
     client = PuregymAPIClient()
@@ -25,4 +26,10 @@ if __name__ == '__main__':
     	df_disk['attendanceTime'] = pd.to_datetime(df_disk['attendanceTime'])
     	df = pd.concat([df, df_disk], sort=False, ignore_index=True).sort_values(['attendanceTime', 'lastRefreshed']).drop_duplicates('attendanceTime', 'last')
     df.to_csv(args.filename)
+
+    if args.show:
+        import matplotlib.pyplot as plt
+        df.plot('attendanceTime', 'totalPeopleInGym')
+        plt.axhline(client.get_gym_attendance(args.gym)[0], c='r')
+        plt.show()
     print(f'{args.filename}')
